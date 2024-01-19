68fa046f9d14f65dff814ee784f3b67f48e18922
GraysonCox <graysoncox98@gmail.com>
2024-01-19 12:49:33 -0600

    Slight improvement

--- src/looper.py
+++ src/looper.py
@@ -6,36 +6,28 @@ from enum import Enum
 SAMPLE_RATE = 44100
 CHUNK_SIZE = 1024
 MAX_CHUNKS_PER_TRACK = 10000
+MAX_TRACKS = 10
 
 
 class State(Enum):
     EMPTY = 0
-    RECORDING = 1
+    RECORDING_FIRST_TRACK = 1
     PLAYING = 2
+    RECORDING_SUBSEQUENT_TRACK = 3
+
+
+class Track:
+    audio = np.zeros([CHUNK_SIZE * MAX_CHUNKS_PER_TRACK, CHUNK_SIZE], dtype=np.int16)
 
 
 class Looper:
     def __init__(self):
-        self.pa = pyaudio.PyAudio()
         self.state = State.EMPTY
-        self.recording = np.zeros([CHUNK_SIZE * MAX_CHUNKS_PER_TRACK, CHUNK_SIZE], dtype=np.int16)
         self.current_chunk = 0
-        self.last_chunk = MAX_CHUNKS_PER_TRACK
+        self.last_chunk = MAX_CHUNKS_PER_TRACK - 1
 
-        def _looping_callback(in_data, _frame_count, _time_info, _status):
-            if self.state == State.EMPTY:
-                return (np.zeros([CHUNK_SIZE], dtype=np.int16), pyaudio.paContinue)
-
-            if self.state == State.RECORDING:
-                current_rec_buffer = np.copy(np.frombuffer(in_data, dtype=np.int16))
-                self.recording[self.current_chunk, :] = current_rec_buffer
-                output_chunk = np.zeros([CHUNK_SIZE], dtype=np.int16)
-            elif self.state == State.PLAYING:
-                output_chunk = self.recording[self.current_chunk : self.current_chunk + CHUNK_SIZE]
-
-            self.current_chunk = (self.current_chunk + 1) % self.last_chunk
-            return (output_chunk, pyaudio.paContinue)
-
+        self.pa = pyaudio.PyAudio()
+        self.tracks = [Track() for _ in range(0, MAX_TRACKS)]
         self.looping_stream = self.pa.open(
             format=pyaudio.paInt16,
             channels=1,
@@ -46,23 +38,52 @@ class Looper:
             output_device_index=1,
             frames_per_buffer=CHUNK_SIZE,
             start=True,
-            stream_callback=_looping_callback,
+            stream_callback=lambda in_data, _1, _2, _3: self._looping_callback(in_data),
         )
 
     def toggle_recording(self):
         if self.state == State.EMPTY:
-            self.state = State.RECORDING
-        elif self.state == State.RECORDING:
+            self.state = State.RECORDING_FIRST_TRACK
+        elif self.state == State.RECORDING_FIRST_TRACK:
             self.state = State.PLAYING
             self.last_chunk = self.current_chunk
             self.current_chunk = 0
         elif self.state == State.PLAYING:
-            self.state = State.RECORDING
+            self.state = State.RECORDING_FIRST_TRACK  # TODO: Add new state.
 
     def clear(self):
+        self.state = State.EMPTY
+        self.current_chunk = 0
+        self.last_chunk = MAX_CHUNKS_PER_TRACK - 1
+
+        self.tracks = [Track() for i in range(0, MAX_TRACKS)]
+
+    def close(self):
         self.looping_stream.close()
         self.pa.terminate()
-        self.state = State.EMPTY
-        self.recording = np.zeros([CHUNK_SIZE], dtype=np.int16)
-        self.current_chunk = 0
-        self.last_chunk = MAX_CHUNKS_PER_TRACK
+
+    def _looping_callback(self, in_data):
+        # Do nothing if EMPTY.
+        if self.state == State.EMPTY:
+            return (np.zeros([CHUNK_SIZE], dtype=np.int16), pyaudio.paContinue)
+
+        # Do recording if desired.
+        if self.state in [
+            State.RECORDING_FIRST_TRACK,
+            State.RECORDING_SUBSEQUENT_TRACK,
+        ]:
+            self.tracks[0].audio[self.current_chunk, :] = np.copy(
+                np.frombuffer(in_data, dtype=np.int16)
+            )
+
+        # Get desired audio for output.
+        if self.state in [State.PLAYING, State.RECORDING_SUBSEQUENT_TRACK]:
+            output_chunk = self.tracks[0].audio[
+                self.current_chunk : self.current_chunk + CHUNK_SIZE
+            ]
+        else:
+            output_chunk = np.zeros([CHUNK_SIZE], dtype=np.int16)
+
+        # Increment counter.
+        self.current_chunk = (self.current_chunk + 1) % self.last_chunk
+        return (output_chunk, pyaudio.paContinue)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import looper
 
 if __name__ == "__main__":
-
-    my_looper = looper.Looper()
+    the_looper = looper.Looper()
     input("Press ENTER to start recording.")
-    my_looper.toggle_recording()
+    the_looper.toggle_recording()
     input("Press ENTER to start looping.")
-    my_looper.toggle_recording()
+    the_looper.toggle_recording()
     input("Press ENTER to quit.")
-    my_looper.clear()
+    the_looper.close()
